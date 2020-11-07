d00fb275da72c9c1a86e538d5d8ee1fcc52486f8
IngAgustinAlmada <agustin.poli2012@gmail.com>
2020-11-06 22:31:04 -0300

    1.02 solved

---
+++ game.js
@@ -0,0 +1,33 @@
+var canvas = null,
+    ctx = null;
+var x = 50,
+    y = 50;
+
+function paint(ctx) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+    //ctx.strokeStyle = "#0f0";
+    //ctx.strokeRect(x, y, 10, 10);
+}
+function act(){
+    x += 1;
+    y += 1;
+    if (x > canvas.width || y > canvas.height) {
+        x = 0;
+        y = 0;
+        }
+    }
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+    }
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    run();
+}
+window.addEventListener('load', init, false);
+
